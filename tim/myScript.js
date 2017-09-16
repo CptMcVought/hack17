837422bcddbe5948042663bae9eee15beb7bea91
fischeti <tim.fischer@bluemail.ch>
2017-09-16 09:19:51 +0200

    hoi

---
+++ tim/myScript.js
@@ -0,0 +1,10 @@
+function geocode_weather(latitude,longitude) {
+	var link = "https://api.weather.com/v1/geocode/" + latitude + "/" + longitude + "/forecast/daily/3day.json?apiKey=626505b9091f4982a505b9091f798235&units=m"
+	var Httpreq = new XMLHttpRequest();
+	Httpreq.open("GET",link,false);
+	Httpreq.send(null);
+	obj_text = Httpreq.responseText;
+	obj_json = JSON.parse(obj_text);
+
+	return JSON.stringify(obj_json.forecasts[0].day.temp);
+}
