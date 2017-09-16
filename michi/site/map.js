fdd25e7f6115d610b82fb33e671d32cd4e8115e4
CptMcVought <info@michiganz.ch>
2017-09-16 02:08:52 +0200

    BEicüling

---
+++ michi/site/map.js
@@ -0,0 +1,35 @@
+
+      function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 47.39, lng: 8.55}
+        });
+        directionsDisplay.setMap(map);
+
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+      }
+
+      
+
+      
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var start = {lat: 47.39, lng: 8.55};
+        directionsService.route({
+          origin: start,
+          destination: new google.maps.LatLng(47.390944,8.70444),
+          travelMode: 'BICYCLING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
