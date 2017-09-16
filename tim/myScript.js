f7921c31cbdcc62621c0bca91af7b3937cea46b2
fischeti <tim.fischer@bluemail.ch>
2017-09-16 13:04:42 +0200

    hoi

--- tim/myScript.js
+++ tim/myScript.js
@@ -1,4 +1,4 @@
-function geocode_weather(latitude,longitude,date) {
+function geocode_weather(latitude,longitude) {
 	var link = "https://api.weather.com/v1/geocode/" + latitude + "/" + longitude + "/forecast/daily/10day.json?apiKey=626505b9091f4982a505b9091f798235&units=m";
 	console.log(link);
 	var Httpreq = new XMLHttpRequest();
@@ -7,7 +7,7 @@ function geocode_weather(latitude,longitude,date) {
 	obj_text = Httpreq.responseText;
 	obj_json = JSON.parse(obj_text);
 
-	var day = date.slice(0,2);
+	/*var day = date.slice(0,2);
 	var month = date.slice(3,5);
 	var year = date.slice(6,10);
 
@@ -18,7 +18,7 @@ function geocode_weather(latitude,longitude,date) {
 	for (i in obj_json.forecasts) {
 		document.write(obj_json.forecasts[i].fcst_valid_local);
 	}
-
+	*/
 	var prob = obj_json.forecasts[0].day.pop;
 	var precipitation = obj_json.forecasts[0].day.qpf;
 
