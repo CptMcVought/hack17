15230bf7de5c0ed67911218d0779dd49095739d4
fischeti <tim.fischer@bluemail.ch>
2017-09-16 19:30:38 +0200

    hoi

--- michi/bootstrap/javascript/magic.js
+++ michi/bootstrap/javascript/magic.js
@@ -199,7 +199,6 @@ function getTheWayPoints(response, directionsService) {
     }
     distanceStuff(directionsService);
     for (var j = 0; j < weatherPoint.length; j++) {
-        console.log(weatherPoint[j] + "," + wayPointLat[numbOfWeatherpoints*j] + "," + wayPointLng[numbOfWeatherpoints*j]);
         routevalue += weatherPoint[j];
     }
     distanceStuff(750, directionsService);
@@ -235,8 +234,6 @@ function geocode_weather(latitude,longitude) {
         var prob = obj_json.forecasts[1].day.pop;
         var precipitation = obj_json.forecasts[1].day.qpf;
     }
-
-    console.log(prob, precipitation, prob*precipitation);
 
     return {probability: prob, quantity: precipitation, expectation: prob*precipitation/100};
 }
